a38bf257eb106aeefde37af876592cd90df17d7f
GrimCreeperyt <grimcreeperyt@gmail.com>
2024-05-13 21:33:41 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// Races sorted by source for source restricting later, no subraces
+const raceAI = ['Verdan'];
+const raceEEPC = ['Aarakocra', 'Genasi'];
+const raceEE = ['Gnoll']
+/*
+SUBRACES/VARIANTS
+EEPC:
+    Genasi:
+        Fire Genasi, Earth Genasi, Air Genasi, Water Genasi
+EE:
+    Aasimar:
+        (Court)*, (Seeker)*, (Fernian)*, Mabaran*
+    Elf:
+        Aereni Elf
+    Dwarf:
+        Ruinbound Dwarf
+    Goblin:
+        Dhakaani Golin'dar
+    Bugbear:
+        Dhakaani Guul'dar
+    Triton:
+        (Kalamer)*
+EGW:
+    Dragonborn:
+        (Draconblood), (Ravenite)
+ERLW:
+    Dwarf:
+        (Mark of Warding)
+    Elf:
+        (Mark of Shadow)
+    Gnome:
+        (Mark of Scribing)
+    Goblin:
+        Goblin
+
+*Check spreadsheets for server-specific errata
+*/
