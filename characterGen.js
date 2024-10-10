63c0dc119a8ea923d958f482361507fb05e1cd4f
GrimCreeperyt <grimcreeperyt@gmail.com>
2024-10-09 20:26:55 -0400

    the... concept... its been proofed... i could cry

--- characterGen.js
+++ characterGen.js
@@ -1,15 +1,19 @@
 // Races sorted by source for source restricting later, no subraces
 const raceAI = ['Verdan']
 const raceEEPC = ['Aarakocra', 'Genasi']
-const raceERLW = ['Bugbear', 'Changeling*', 'Goblin', 'Hobgoblin', 'Kalashtar', 'Orc*', 'Shifter', 'Warforged']
+const raceEGW = ['Dragonborn']
+const raceERLW = ['Bugbear', 'Changeling', 'Dwarf', 'Elf', 'Gnome', 'Goblin', 'Half Elf', 'Half-Orc', 'Halfling', 'Hobgoblin', 'Human', 'Kalashtar', 'Orc', 'Shifter', 'Warforged']
 const raceUARoR = ['Viashino']
-const raceGGR = ['Loxodon', 'Simic Hybrid', 'Vedalken*']
-const raceMOT = ['Centaur*', 'Leonin', 'Minotaur', 'Satyr', 'Triton']
-const racePHB = ['Dragonborn', '']
+const raceGGR = ['Loxodon', 'Simic Hybrid', 'Vedalken']
+const raceMOT = ['Centaur', 'Leonin', 'Minotaur', 'Satyr', 'Triton']
+const raceMTF = ['Dwarf', 'Elf', 'Gith', 'Gnome', 'Tiefling']
+const racePHB = ['Dragonborn', 'Dwarf', 'Elf', 'Gnome', 'Half Elf', 'Half Orc', 'Halfling', 'Human', 'Tiefling']
+const raceSCAG = ['Tiefling', 'Half Elf']
 // after filtering sourcebooks:
-let race = [concat(raceAI, raceEEPC, raceERLW)]
+let race = []
+race = race.concat(raceAI, raceEEPC, raceEGW, raceERLW, raceUARoR, raceGGR, raceMOT, raceMTF, racePHB, raceSCAG)
 race = [...new Set(race)]
-
+console.log(race)
 /*const feats = [{name: 'feat1'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}, {name: 'feat2'}];
 feats.forEach((element) => {
     switch (element.name) {
